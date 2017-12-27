d9d96a66b1ca4c749cfab69fc4712270d3352ec1
apvanzanten <ap.vanzanten@gmail.com>
2017-12-27 22:29:18 +0100

    Added a buffered serial io class.

---
+++ src/io/buffered_serial.hpp
@@ -0,0 +1,80 @@
+#ifndef BUFFERED_SERIAL_HPP
+#define BUFFERED_SERIAL_HPP
+
+#include "../util/circular_buffer.hpp"
+#include "mbed.h"
+#include <array>
+#include <cstdarg>
+#include <cstdio>
+
+namespace sseq {
+  namespace io {
+    template <std::size_t N> class buffered_serial {
+      util::circular_buffer<char, N> buffer;
+      Serial s;
+
+    public:
+      explicit buffered_serial(PinName tx, PinName rx, int baud = 9600)
+          : buffer(), s(tx, rx, baud) {}
+
+      inline bool try_write() {
+        if (!buffer.empty() && s.writeable()) {
+          s.putc(buffer.pop());
+          return true;
+        }
+        return false;
+      }
+
+      inline void flush() {
+        while (!buffer.empty()) {
+          try_write();
+        }
+      }
+
+      inline void clear() { buffer.clear(); }
+
+      inline bool try_putc(char c) {
+        if (buffer.full()) {
+          return false;
+        }
+        buffer.push(c);
+        return true;
+      }
+
+      inline bool try_puts(const char *s) {
+        size_t n = 0;
+        while (s[n] != '\0') {
+          n++;
+        }
+        return buffer.try_push_array(s, n);
+      }
+
+      inline void putc(char c) {
+        while (buffer.full()) {
+          try_write();
+        }
+        buffer.push(c);
+      }
+
+      inline void puts(const char *s) {
+        size_t i = 0;
+        while (s[i] != '\0') {
+          putc(s[i++]);
+        }
+      }
+
+      inline bool try_printf(const char *format, ...) {
+        std::va_list args;
+        va_start(args, format);
+        std::array<char, N> arr;
+        size_t n = std::vsnprintf(arr.data(), buffer.num_free(), format, args);
+        if (n >= 0 && n < buffer.num_free()) {
+          return buffer.try_push_array(arr.data(), n);
+        }
+        return false;
+      }
+    };
+  } // namespace io
+} // namespace sseq
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "sacroseq.hpp"
+#include "io/buffered_serial.hpp"
 
 int main() {
   using namespace sseq;
 
-  Serial pc(midi::pins::TX, midi::pins::RX, midi::BAUD_RATE);
-  pc.printf("hello!\n");
+  io::buffered_serial<32> pc(midi::pins::TX, midi::pins::RX, midi::BAUD_RATE);
+  pc.try_printf("hello!\n");
+  pc.flush();
 
   alerts::done();
 }
