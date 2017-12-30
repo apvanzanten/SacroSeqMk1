f7cec7943e8a2efa383c5ddc2dd84bcc91e5bebd
apvanzanten <ap.vanzanten@gmail.com>
2017-12-30 17:15:46 +0100

    Set up a Ticker and some basic encoder testing to main.

--- src/main.cpp
+++ src/main.cpp
@@ -3,42 +3,53 @@
 #include "io/grid.hpp"
 #include "sacroseq.hpp"
 
-int main() {
-  using namespace sseq;
+using namespace sseq;
 
-  io::buffered_serial<128> pc(midi::pins::TX, midi::pins::RX, midi::BAUD_RATE);
+DigitalOut board_led(LED1);
+
+io::buffered_serial<128> pc(midi::pins::TX, midi::pins::RX, midi::BAUD_RATE);
+io::grid<grid::NUM_SOURCES, grid::NUM_INPUTS, grid::NUM_OUTPUTS> grid_io(
+    {
+        DigitalOut(grid::pins::SOURCES[0]),
+        DigitalOut(grid::pins::SOURCES[1]),
+        DigitalOut(grid::pins::SOURCES[2]),
+        DigitalOut(grid::pins::SOURCES[3]),
+        DigitalOut(grid::pins::SOURCES[4]),
+        DigitalOut(grid::pins::SOURCES[5]),
+        DigitalOut(grid::pins::SOURCES[6]),
+        DigitalOut(grid::pins::SOURCES[7]),
+    },
+    {DigitalIn(grid::pins::ENC_BUTTON, PullDown), DigitalIn(grid::pins::ENC_A, PullDown),
+     DigitalIn(grid::pins::ENC_B, PullDown), DigitalIn(grid::pins::BUTTON, PullDown)},
+    {DigitalOut(grid::pins::LED)});
+
+void step() {
+  board_led = 1;
+  grid_io.step();
+  pc.try_write();
+  board_led = 0;
+}
+
+int main() {
   pc.try_printf("hello!\n");
   pc.flush();
 
-  io::grid<grid::NUM_SOURCES, grid::NUM_INPUTS, grid::NUM_OUTPUTS> grid_io(
-      {
-          DigitalOut(grid::pins::SOURCES[0]),
-          DigitalOut(grid::pins::SOURCES[1]),
-          DigitalOut(grid::pins::SOURCES[2]),
-          DigitalOut(grid::pins::SOURCES[3]),
-          DigitalOut(grid::pins::SOURCES[4]),
-          DigitalOut(grid::pins::SOURCES[5]),
-          DigitalOut(grid::pins::SOURCES[6]),
-          DigitalOut(grid::pins::SOURCES[7]),
-      },
-      {DigitalIn(grid::pins::ENC_BUTTON, PullDown), DigitalIn(grid::pins::ENC_A, PullDown),
-       DigitalIn(grid::pins::ENC_B, PullDown), DigitalIn(grid::pins::BUTTON, PullDown)},
-      {DigitalOut(grid::pins::LED)});
+  Ticker t;
+  t.attach_us(&step, 50);
 
-  io::encoder encoder;
+  io::encoder enc;
+  const size_t enc_index = 0;
+  const size_t enc_a_index = 1;
+  const size_t enc_b_index = 2;
+  
+  const bool &enc_a_val = grid_io.get_ref(enc_index, enc_a_index);
+  const bool &enc_b_val = grid_io.get_ref(enc_index, enc_b_index);
 
   while (true) {
-    grid_io.step();
-    for (size_t i = 0; i < grid::NUM_SOURCES; i++) {
-      grid_io.set(i, 0, grid_io.get(i, 0));
-
-      pc.try_printf("%d%d%d%d\t", grid_io.get(i, 0), grid_io.get(i, 1), grid_io.get(i, 2),
-                    grid_io.get(i, 3));
-    }
-    encoder.update(grid_io.get(0, 1), grid_io.get(0, 2));
-    pc.try_printf("%d", encoder.peek());
+    enc.update(enc_a_val, enc_b_val);
+    pc.try_printf("%d%d: %d", enc_a_val, enc_b_val, enc.peek());
     pc.try_putc('\n');
-    pc.flush();
+    wait(0.001);
   }
 
   alerts::done();
