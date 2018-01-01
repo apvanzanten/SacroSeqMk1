a0beb33d60192c68bc99b94d237a4ff8597d5284
apvanzanten <ap.vanzanten@gmail.com>
2018-01-01 21:37:22 +0100

    Cleaned up the sequencer class a little bit.

---
+++ src/sequencer.cpp
@@ -0,0 +1,101 @@
+#include "sequencer.hpp"
+
+namespace sseq {
+  void sequencer::move_to_next_step() {
+    int new_step_index_signed_unbounded = step_index + get_step_offset();
+
+    while (new_step_index_signed_unbounded < 0) {
+      new_step_index_signed_unbounded += steps.size();
+    }
+    size_t new_step_index_unbounded = static_cast<size_t>(new_step_index_signed_unbounded);
+    while (new_step_index_unbounded >= steps.size()) {
+      new_step_index_unbounded -= steps.size();
+    }
+    step_index = new_step_index_unbounded;
+    current_repetition = 0;
+  }
+
+  void sequencer::update_gate_state() {
+    switch (gate_state) {
+    case gate_states::initial:
+      if (time_delta >= period) {
+        time_delta = 0;
+
+        note_on(steps[step_index].note);
+
+        switch (steps[step_index].gate) {
+        case gate_mode::hold:
+          gate_state = gate_states::hold_on;
+          break;
+        case gate_mode::pulse_once:
+          gate_state = gate_states::pulse_once_on;
+          break;
+        case gate_mode::pulse_once_then_hold:
+          gate_state = gate_states::pulse_once_then_hold_on;
+          break;
+        case gate_mode::pulse_repeat:
+          gate_state = gate_states::pulse_repeat_on;
+          break;
+        }
+      }
+      break;
+    case gate_states::hold_on:
+      if (time_delta >= gate_time && current_repetition >= steps[step_index].repetitions) {
+        note_off(steps[step_index].note);
+        move_to_next_step();
+        gate_state = gate_states::initial;
+      } else if (time_delta >= period) {
+        move_to_next_repetition();
+      }
+      break;
+    case gate_states::pulse_once_on:
+      if (time_delta >= gate_time) {
+        note_off(steps[step_index].note);
+        gate_state = gate_states::hold_off;
+      }
+      break;
+    case gate_states::hold_off:
+      if (time_delta >= period) {
+        if (current_repetition >= steps[step_index].repetitions) {
+          move_to_next_step();
+          gate_state = gate_states::initial;
+        } else {
+          move_to_next_repetition();
+        }
+      }
+      break;
+    case gate_states::pulse_once_then_hold_on:
+      if (time_delta >= gate_time) {
+        note_off(steps[step_index].note);
+        gate_state = gate_states::pulse_once_then_hold_off;
+      }
+      break;
+    case gate_states::pulse_once_then_hold_off:
+      if (time_delta >= period) {
+        note_on(steps[step_index].note);
+        move_to_next_repetition();
+        gate_state = gate_states::hold_on;
+      }
+      break;
+    case gate_states::pulse_repeat_on:
+      if (time_delta >= gate_time) {
+        note_off(steps[step_index].note);
+        gate_state = gate_states::pulse_repeat_off;
+      }
+      break;
+    case gate_states::pulse_repeat_off:
+      if (time_delta >= period) {
+        move_to_next_repetition();
+        if (current_repetition >= steps[step_index].repetitions) {
+          move_to_next_step();
+          gate_state = gate_states::initial;
+        } else {
+          note_on(steps[step_index].note);
+          gate_state = gate_states::pulse_repeat_on;
+        }
+      }
+      break;
+    }
+  }
+
+} // namespace sseq
--- src/sequencer.hpp
+++ src/sequencer.hpp
@@ -49,115 +49,22 @@ namespace sseq {
 
     inline void note_off(midi::note note) { output_buffer.push(note_message{note, 0}); }
 
-    inline void note_on(midi::note note, std::uint8_t velocity) {
-      output_buffer.push(note_message{note, velocity});
-    }
+    inline std::uint8_t get_velocity() { return (steps[step_index].is_active ? 127 : 0); }
 
-    inline std::uint32_t get_velocity() { return (steps[step_index].is_active ? 127 : 0); }
+    inline void note_on(midi::note note) {
+      output_buffer.push(note_message{note, get_velocity()});
+    }
 
     inline std::int8_t get_step_offset() { return 1; }
 
-    inline void move_to_next_step() {
-      int new_step_index_signed_unbounded = step_index + get_step_offset();
-
-      while (new_step_index_signed_unbounded < 0) {
-        new_step_index_signed_unbounded += steps.size();
-      }
-      size_t new_step_index_unbounded = static_cast<size_t>(new_step_index_signed_unbounded);
-      while (new_step_index_unbounded >= steps.size()) {
-        new_step_index_unbounded -= steps.size();
-      }
-      step_index = new_step_index_unbounded;
-      current_repetition = 0;
-    }
+    void move_to_next_step();
 
     inline void move_to_next_repetition() {
       ++current_repetition;
       time_delta = 0;
     }
 
-    inline void update_gate_state() {
-      switch (gate_state) {
-      case gate_states::initial:
-        if (time_delta >= period) {
-          time_delta = 0;
-
-          note_on(steps[step_index].note, get_velocity());
-
-          switch (steps[step_index].gate) {
-          case gate_mode::hold:
-            gate_state = gate_states::hold_on;
-            break;
-          case gate_mode::pulse_once:
-            gate_state = gate_states::pulse_once_on;
-            break;
-          case gate_mode::pulse_once_then_hold:
-            gate_state = gate_states::pulse_once_then_hold_on;
-            break;
-          case gate_mode::pulse_repeat:
-            gate_state = gate_states::pulse_repeat_on;
-            break;
-          }
-        }
-        break;
-      case gate_states::hold_on:
-        if (time_delta >= gate_time && current_repetition >= steps[step_index].repetitions) {
-          note_off(steps[step_index].note);
-          move_to_next_step();
-          gate_state = gate_states::initial;
-        } else if (time_delta >= period) {
-          move_to_next_repetition();
-        }
-        break;
-      case gate_states::pulse_once_on:
-        if (time_delta >= gate_time) {
-          note_off(steps[step_index].note);
-          gate_state = gate_states::hold_off;
-        }
-        break;
-      case gate_states::hold_off:
-        if (time_delta >= period) {
-          if (current_repetition >= steps[step_index].repetitions) {
-            move_to_next_step();
-            gate_state = gate_states::initial;
-          } else {
-            move_to_next_repetition();
-          }
-        }
-        break;
-      case gate_states::pulse_once_then_hold_on:
-        if (time_delta >= gate_time) {
-          note_off(steps[step_index].note);
-          gate_state = gate_states::pulse_once_then_hold_off;
-        }
-        break;
-      case gate_states::pulse_once_then_hold_off:
-        if (time_delta >= period) {
-          note_on(steps[step_index].note, get_velocity());
-          move_to_next_repetition();
-          gate_state = gate_states::hold_on;
-        }
-        break;
-      case gate_states::pulse_repeat_on:
-        if (time_delta >= gate_time) {
-          note_off(steps[step_index].note);
-          gate_state = gate_states::pulse_repeat_off;
-        }
-        break;
-      case gate_states::pulse_repeat_off:
-        if (time_delta >= period) {
-          move_to_next_repetition();
-          if (current_repetition >= steps[step_index].repetitions) {
-            move_to_next_step();
-            gate_state = gate_states::initial;
-          } else {
-            note_on(steps[step_index].note, get_velocity());
-            gate_state = gate_states::pulse_repeat_on;
-          }
-        }
-        break;
-      }
-    }
+    void update_gate_state();
 
   public:
     inline void set_bpm(size_t new_bpm) { period = (60 * MAIN_CLOCK_FREQUENCY_HZ) / new_bpm; }
@@ -174,7 +81,6 @@ namespace sseq {
       }
 
       update_gate_state();
-
       return !(output_buffer.empty());
     }
 
